21f1ce696cae51c97b1027f6cca13df5b7b68457
nbalcarc <nathan.balcarcel@wsu.edu>
2023-11-06 00:11:26 -0800

    Created interface file for test module

---
+++ src/c_testing/Example.pyi
@@ -0,0 +1,14 @@
+"""Test module for extending Python with C."""
+
+def findPrimes(num1: int, num2: int) -> int:
+    """Finds all primes within the range provided."""
+
+def return_two() -> tuple[int, int]:
+    """Returns two integers."""
+
+class Custom():
+    """Basic experimentation"""
+
+    number: float 
+    number1: float
+

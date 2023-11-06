a3dd36bdb0d7e5debe46f3720bd3ce908d9e45ce
eddsteiner <122760865+eddsteiner@users.noreply.github.com>
2023-11-06 15:40:37 -0800

    Add files via upload

--- src/engine.py
+++ src/engine.py
@@ -12,6 +12,8 @@ class ChineseCheckersEngine:
         self.board2 = np.zeros(81) # 9x9 board
         self.objective_zone1 = [0, 1, 2, 3, 9, 10, 11, 18, 19, 27]
         self.objective_zone2 = [53, 61, 62, 69, 70, 71, 77, 78, 79, 80]
+        self.moves = [-1, -9, 8, -8, 9, 1]
+        self.jumps = [-2, -18, 16, -16, 18, 2]
         self.current_player = True  # Start with Player 1
         self.player1 = player1
         self.player2 = player2
@@ -60,17 +62,86 @@ class ChineseCheckersEngine:
     def is_valid_move(self, start_pos: int, action: int) -> bool:
         """Returns: True = valid move, False = invalid move."""
         # Implement the rules to check if a move is valid
+        
+
+        if action not in self.moves: #first checks if the action value is even in the list of valid move values
+            return False
+         #check if the space is open
+        elif self.board1[start_pos + action] != 0: 
+            return False # or i could potentially make a isValid_Jump() function that could run here
+        else:
+            #check first side -----------------------------------
+            if start_pos % 9 == 0:
+                #check the 0 corner that only allows 2 moves
+                if start_pos == 0:
+                    if action == (1 or 9):
+                        return True
+                    else:
+                        return False
+                #check the 72 corner that only allows 3 moves
+                elif start_pos == 72:
+                    if action == (1 or -9 or -8):
+                        return True 
+                    else: 
+                        return False
+                    
+                elif action == (8 or (-1)):
+                    return False
+                else:
+                    return True
+            
+             #check second side -----------------------------------
+            elif (start_pos + 1) % 9 == 0:
+                #check the 80 corner that only allows 2 moves
+                if start_pos == 80:
+                    if action == (-1 or -9):
+                        return True
+                    else:
+                        return False
+                #check the 8 corner that only allows 3 moves
+                elif start_pos == 8:
+                    if action == (-1 or 9 or 8):
+                        return True 
+                    else: 
+                        return False
+                elif action == (1 or (-8)):
+                    return False
+                else:
+                    return True 
+                
+            #check  third side ---------------------------------  
+            elif start_pos in range(72, 81):
+                if action == (9 or 8):
+                    return False
+                else:
+                    return True
+            #check fourth side -----------------------------------
+            elif start_pos in range(0-9):
+                if action == ((-9) or (-8)):
+                    return False
+                else: return True
+            
+
+
+
         return True 
 
 
     def make_move(self, start_pos: int, action: int, player: bool) -> bool:
         """Returns: True = made move, False = invalid move."""
-        is_valid = self.is_valid_move(start_pos, action)
-        if not is_valid:
-            return False
-        # Make a move on the board if it's valid
-        return True
-
+        #check for valid piece picked
+        if(self.current_player):
+            if self.board1[start_pos] != 1:     #check if player 1 picked player 1 piece
+                is_valid = False                #if no then piece move is not valid
+            else:
+                is_valid = self.is_valid_move(start_pos, action)    #else check if the action is valid
+        else:
+            if self.board1[start_pos] != 2:     #do the same for player 2
+                is_valid = False
+            else:
+                is_valid = self.is_valid_move(start_pos, action)
+                
+        return is_valid
 
     def retrieve_board(self, player: bool) -> npt.NDArray[np.float64]:
         """Returns the board for the specified player."""

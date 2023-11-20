2f16adb440284ba412b07d8ba550f1c6011cb6da
nbalcarc <nathan.balcarcel@wsu.edu>
2023-11-20 15:47:50 -0800

    Kamiak works with Python, C, and CUDA

--- src/build.sh
+++ src/build.sh
@@ -1,6 +1,14 @@
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)
 nvcc --ptxas-options=-v --compiler-options '-fPIC' -o libmaxmul.so --shared maxmul.cu &&
 gcc -shared -o neat.so -fPIC neat.c libmaxmul.so &&
-LD_LIBRARY_PATH=. python testing.py
+python3 testing.py
+
+#LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)
+#echo hi2
+#nvcc --ptxas-options=-v --compiler-options '-fPIC' -o libmaxmul.so --shared maxmul.cu
+#echo hi1
+#python3 testing.py
+#echo hi
 
 
 

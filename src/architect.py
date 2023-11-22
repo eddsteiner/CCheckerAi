fa6235c4fbf4822113562fc1067a9bf33b5f6bfb
nbalcarc <nathan.balcarcel@wsu.edu>
2023-11-21 16:53:28 -0800

    Re-added Architect class and adjusted NEAT responsibilities

---
+++ src/architect.py
@@ -0,0 +1,77 @@
+import numpy as np
+import numpy.typing as npt
+
+from neat import NEAT, Creature
+
+
+GENERATION_SIZE = 500
+
+
+class Architect:
+    """Manages and evolves generations of Creatures."""
+
+
+    def __init__(self):
+        self.neat = NEAT()
+        """Contains all the creatures in this generation."""
+
+
+    def save_generation(self) -> bool:
+        """
+        TODO Saves the current sorted generation to a file.
+
+        Returns True if process succeeded, False otherwise.
+        """
+
+        # ensure the folder where we save generations is created
+        gen = self.neat.generation_number
+        ranking = self._ranking()
+        file_name = f"generations/gen_{gen}.gen"
+        return self.neat.save_generation(ranking.ctypes.data, file_name) #save
+
+
+    def load_generation(self, file_name: str) -> bool:
+        """
+        Loads a sorted generation from a file.
+
+        Returns True if process succeeded, False otherwise.
+        """
+
+        return self.neat.load_generation(file_name)
+
+
+    def evolve(self) -> bool:
+        """
+        Plays through one generation of evolution.
+
+        Returns True if process succeeded, False otherwise.
+        """
+
+        return self._evolution(self._ranking())
+
+
+    def _ranking(self) -> npt.NDArray[np.int32]:
+        """
+        TODO Runs a tournament for the whole generation.
+
+        Returns rankings as a numpy array.
+        """
+
+        rankings = np.zeros(GENERATION_SIZE, dtype = np.int32)
+
+        #do all the tournament things here
+
+        return rankings
+
+
+    def _evolution(self, rankings: npt.NDArray[np.int32]) -> bool:
+        """
+        Calls NEAT's evolve function, enacting the rankings and repopulation.
+
+        Returns True if process succeeded, False otherwise.
+        """
+
+        return self.neat.evolve(rankings.ctypes.data)
+
+
+
--- src/neat.pyi
+++ src/neat.pyi
@@ -1,22 +1,24 @@
 """NEAT Library Implemented in C and CUDA"""
 
-import ctypes
-
 
 class NEAT:
-    """Manages all functionality for NEAT."""
+    """Manages all functionality for a NEAT generation."""
 
-    def get_current_best(self) -> Creature: """Returns the best Creature in the current generation."""
+    generation_number: int
+    """The current generation's number."""
+
+    def get_current_best(self) -> Creature:
+        """Returns the best Creature in the current generation."""
 
     def get_current_generation(self) -> list[Creature]:
         """Returns all Creatures in this generation."""
 
-    def get_generation_number(self) -> int:
-        """Returns the current generation's number."""
+    #def get_generation_number(self) -> int:
+    #    """Returns the current generation's number."""
 
-    def save_generation(self, file: str) -> bool:
+    def save_generation(self, ranking: int, file: str) -> bool:
         """
-        Saves the current generation into the specified file.
+        Saves the current sorted generation into the specified file.
 
         Returns True if process succeeded, False otherwise.
         """
@@ -29,14 +31,19 @@ class NEAT:
         Returns True if process succeeded, False otherwise.
         """
 
-    def evolve(self) -> None:
-        """Evolves one generation."""
+    def evolve(self, ranking: int) -> bool:
+        """
+        Evolves one generation.
+        Receives a pointer to the rankings.
+
+        Returns True if process succeeded, False otherwise.
+        """
 
 
 class Creature:
     """An individual Creature which can play Chinese Checkers."""
 
-    def calculate(self, board: ctypes.c_long, output_buf: ctypes.c_long) -> None:
+    def calculate(self, board: int, output_buf: int) -> None:
         """
         Takes a pointer to a board and an output buffer, and
         fills the buffer with calculated confidences for each move.
@@ -45,10 +52,13 @@ class Creature:
         The output buffer should be of size x.
         """
 
+
+
+
+
+
 def maxmul(a: int, b: int, c: int, size: int) -> None:
     """Temporary thing to test CUDA"""
-
-
 
 
 

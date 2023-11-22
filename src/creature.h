f479d9c14f641098bfcf74d77c119a217e3bf274
nbalcarc <nathan.balcarcel@wsu.edu>
2023-11-22 04:49:09 -0800

    Removed unnecessary includes

--- src/creature.h
+++ src/creature.h
@@ -4,11 +4,6 @@
 
 #define PY_SSIZE_T_CLEAN
 #include <python3.11/Python.h>
-#include <python3.11/structmember.h>
-#include <python3.11/floatobject.h>
-#include <python3.11/modsupport.h>
-#include <stdint.h>
-#include <stdlib.h>
 
 
 extern PyTypeObject PyCreature;
